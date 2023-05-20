e07ca688e6213dfd0a590d4d92978dd50e4a9a39
monicaz2 <123012325+monicaz2@users.noreply.github.com>
2023-05-20 17:15:21 -0500

    Experimenting

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ def main():
     
     if 'uploaded_file' not in st.session_state or not st.session_state.uploaded_file:
         st.session_state.uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
-        print(st.session_state.uploaded_file)
+        st.text(st.session_state.uploaded_file)
     
     if 'uploaded_file' in st.session_state and st.session_state.uploaded_file:
         st.text("Input Image: ")
