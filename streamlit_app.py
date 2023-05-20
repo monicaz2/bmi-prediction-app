1f90e6fb30e2b1d91b1d3005ce54094269d94ffd
monicaz2 <123012325+monicaz2@users.noreply.github.com>
2023-05-20 16:57:58 -0500

    Adding state update callback for is_file_uploaded

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
+def is_file_uploaded_state_update(updated_value):
+    st.session_state.is_file_uploaded = updated_value
+
 def main():
     st.title("Monica's BMI Prediction App")
     st.text("Welcome to Monica's BMI Prediction Application. It's simple to use!")
     st.text("Just upload a picture of your face and my model will predict your BMI.")
     
-    if 'uploaded_file' not in st.session_state or not st.session_state.uploaded_file:
-        st.session_state.uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
+    if 'is_file_uploaded' not in st.session_state or not st.session_state.is_file_uploaded:
+        st.session_state.uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'], on_change=is_file_uploaded_state_update(1))
     else:
         st.text("Input Image: ")
         st.image(st.session_state.uploaded_file)
