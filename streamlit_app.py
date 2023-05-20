2006ad31e418dada0f1f21ad4404c8c1485b2e08
monicaz2 <123012325+monicaz2@users.noreply.github.com>
2023-05-20 16:38:01 -0500

    Fixing text display

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ st.write('Hello world!')
 
 def main():
     st.title("Monica's BMI Prediction App")
-    st.text("Welcome to Monica's BMI Prediction Application. It's simple to use! Just upload a picture of your face and my model will predict your BMI.")
+    st.text("Welcome to Monica's BMI Prediction Application. It's simple to use!")
+    st.text("Just upload a picture of your face and my model will predict your BMI.")
     
     if 'is_file_uploaded' not in st.session_state or not st.session_state.is_file_uploaded:
         uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
