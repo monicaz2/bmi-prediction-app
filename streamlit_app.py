ced96066ad5b819184f05547c2a806044e995740
monicaz2 <123012325+monicaz2@users.noreply.github.com>
2023-05-20 16:50:21 -0500

    Fixing statefulness

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 import streamlit as st
 
-uploaded_file = None
-
 def main():
     st.title("Monica's BMI Prediction App")
     st.text("Welcome to Monica's BMI Prediction Application. It's simple to use!")
     st.text("Just upload a picture of your face and my model will predict your BMI.")
     
-    if 'is_file_uploaded' not in st.session_state or not st.session_state.is_file_uploaded:
-        uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
-        
-    if uploaded_file is not None:
-        st.image(uploaded_file)
-        st.session_state.is_file_uploaded = 1
+    if 'uploaded_file' not in st.session_state or not st.session_state.uploaded_file:
+        st.session_state.uploaded_file = st.file_uploader("Upload your image here...", type=['png', 'jpeg', 'jpg'])
+    else:
+        st.text("Input Image: ")
+        st.image(st.session_state.uploaded_file)
   
 
 if __name__ == "__main__":
